666bc57aa34fec8e8c2401f3777fa94669062be7
Jascha Beste <bestejascha@gmail.com>
2019-10-17 12:49:10 +0200

    Updates

---
+++ precache-manifest.fb19f99d16cb603d1a55e0ee12abc538.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0387ec481ef1e52c5e6eac577f243465",
+    "url": "/earthquakerice/index.html"
+  },
+  {
+    "revision": "8e3fd937cfdca72991fb",
+    "url": "/earthquakerice/static/css/main.fc76f0dd.chunk.css"
+  },
+  {
+    "revision": "8c8cdd244036a70c2faa",
+    "url": "/earthquakerice/static/js/2.1c3cee83.chunk.js"
+  },
+  {
+    "revision": "8e3fd937cfdca72991fb",
+    "url": "/earthquakerice/static/js/main.72f85c9d.chunk.js"
+  },
+  {
+    "revision": "505effd087000bfd7962",
+    "url": "/earthquakerice/static/js/runtime-main.efd82eea.js"
+  }
+]);
